b4a1b542db8fc21804646c9547cb38f4959006c0
邓新 <dengxin@dakale.net>
2021-12-01 18:02:37 +0800

    no message

--- config/router.config.js
+++ config/router.config.js
@@ -4,4 +4,9 @@ export default [
     name: '路由',
     component: './index',
   },
+  {
+    path: '/register',
+    name: '签到',
+    component: './register',
+  },
 ];
---
+++ src/pages/register/scene/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+class Index extends React.Component {
+  constructor() {
+    super(...arguments);
+    this.state = {};
+  }
+}
